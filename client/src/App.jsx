6f6fca148454877ad579210344304c87d2f4bdc9
LennyOff <lenlepetitfilou@gmail.com>
2024-01-16 09:49:59 +0100

    ajout textures

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,13 +3,15 @@ import { Header,Hero,Projects,Music,Contact,Footer } from './components/index'
 
 function App() {
   return (
-    <div className='maindiv'> {/*Manque les effets*/}
-      <Header/> {/* Manque le backend Login */}
-      <Hero/> {/* 100% fini */}
-      <Music/> {/* 100% fini */}
-      <Projects/> {/* Manque page "see more" */}
-      <Contact/> {/* Manque le backend */}
-      <Footer/> {/* 100% fini */}
+    <div className='backgroundDetails'>
+      <div className='maindiv'> {/*Manque les effets*/}
+        <Header/> {/* Manque le backend Login */}
+        <Hero/> {/* 100% fini */}
+        <Music/> {/* 100% fini */}
+        <Projects/> {/* Manque page "see more" */}
+        <Contact/> {/* Manque le backend */}
+        <Footer/> {/* 100% fini */}
+      </div>
     </div>
   )
 }
